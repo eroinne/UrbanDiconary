25bf5fecd4d7a710f8fa329eae610251a79ef244
Ero <lambeybaptiste@gmail.com>
2023-09-08 14:30:45 +0200

    Initial Commit

---
+++ Memeizer/MemeizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeizerApp.swift
+//  Memeizer
+//
+//  Created by Ero on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemeizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
